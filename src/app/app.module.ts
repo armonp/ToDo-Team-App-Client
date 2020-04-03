9a038ba49d8dc3736decd6a7a768f2dff644919e
max-student <max-student@maxtrain.com>
2020-04-03 16:17:25 -0400

    generate components/services/classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { LoginComponent } from './user/login/login.component';
+import { TodoCreateComponent } from './todo/todo-create/todo-create.component';
+import { TodoEditComponent } from './todo/todo-edit/todo-edit.component';
+import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
+import { TodoAssignComponent } from './todo/todo-assign/todo-assign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    LoginComponent,
+    TodoCreateComponent,
+    TodoEditComponent,
+    TodoDetailComponent,
+    TodoAssignComponent
   ],
   imports: [
     BrowserModule,
